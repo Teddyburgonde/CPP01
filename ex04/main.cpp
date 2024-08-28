c57eddce3a956e9b3f7e36381ed28607dd77a17a
Teddyburgonde <bandama.teddy@gmail.com>
2024-08-28 12:21:15 +0200

    to do norme and create class

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tebandam <tebandam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 15:50:31 by tebandam          #+#    #+#             */
-/*   Updated: 2024/08/27 16:28:18 by tebandam         ###   ########.fr       */
+/*   Updated: 2024/08/28 12:12:25 by tebandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,27 +24,6 @@ using namespace std;
 // https://github.com/madebypixel02/CPP-Module-01/blob/main/ex04/main.cpp
 
 
-
-/*!
-(Déjà fait) : Ouvrir le fichier d'entrée et vérifier qu'il s'ouvre correctement.
-(Déjà fait) : Lire le fichier ligne par ligne.
-
-(À faire) : Remplacer toutes les occurrences de 
-s1 par s2 dans chaque ligne.
-
-(À faire) : Ouvrir un nouveau 
-fichier pour écrire le résultat modifié.
-
-(Déjà fait en partie) : Fermer le fichier 
-d'entrée. 
-
-(À faire) Fermer également le fichier de sortie.
-
-(À faire) : Ajouter une gestion 
-des erreurs pour les chaînes s1 et s2, et créer
-des tests pour vérifier 
-le bon fonctionnement du programme.
-*/
 
 /*
 
@@ -73,89 +52,47 @@ reconstruire la ligne avec les modifications nécessaires.
 // std::string::npos si la chaine n'as pas etait trouvée
 
 
-//int ft_open(std::string name_of_file, char *s1, char *s2)
-// & permet de ne pas creer de nouvelle copie dans la memoire
-// void ft_open(const std::string& name_of_file, char *s1)
-// {
-// 	std::string line;
-// 	int pos_s1;
-// 	ifstream inputFile(name_of_file.c_str());
-
-// 	if (!inputFile.is_open())
-// 	{
-// 		cerr << "Error opening the file!" << endl;
-// 		return ;
-// 	}
-// 	while (getline(inputFile, line))
-// 	{
-// 		//cout << line << endl;
-
-// 		pos_s1 = line.find(s1);
-// 		if (pos_s1 != 0)
-// 		{
-// 			cout << "find: " << line << endl;
-// 		}
-// 	}
-// 	inputFile.close();
-// }
-
-// int	main(void)
-// {
-// 	char s1[] = "bonjour";
-// 	ft_open(std::string("abc.txt"), s1);
-// }
 
 
 
-// int	print_error(std::string message)
-// {
-// 	cerr << message << endl;	
-// 	return (1);
-// }
 
 
-// int	main(int argc, char **argv)
-// {
-// 	std::ifstream infile;
-// 	std::ofstream outfile;
-// 	std::string file;
-// 	std::string line;
-// 	std::string s1;
-// 	std::string s2;
-// 	size_t pos;
-// 	if (argc != 4)
-// 		return (print_error("Wrong number of arguments"));
-// 	file = argv[1];
-// 	s1 = argv[2];
-// 	s2 = argv[3];
-// 	// ouverture du fichier et c_str converti en char* car open veut un char *
-// 	infile.open(file.c_str());
-// 	if (!infile.is_open())
-// 		return (print_error("Error opening the file!"));
-// 	while (getline(infile, line))
-// 	{
-// 		if (pos)
-// 		//cout << line << endl;
-// 	}
-// 	infile.close();
-// }
+
+// 1. input_file = argv[1] s1 = argv[2] s2 = argv[3]
+// 2. Ouvrir l'infile 
+// 3. Verif de l'open infile
+// 4. Lire le contenu du fichier 
+// 5. Recherche le contenu de s1 dans le fichier avec find
+// 6. Remplacer s1 par le contenu de s2 
+// 7. Ecrire le contenu de line dans le fichier outfile.
+// 8. donner a outfile un no
+
+
+
+
 
 #include <iostream>
 #include <fstream>
 #include <string>
 
-int print_error(const std::string &message) {
+int print_error(const std::string &message) 
+{
     std::cerr << message << std::endl;
     return 1;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
     std::ifstream infile;
     std::ofstream outfile;
     std::string input_file;
+    std::string output_file;
     std::string s1;
     std::string s2;
-
+    size_t  pos;
+    std::string line;
+    
+    pos = 0;
     if (argc != 4)
         return print_error("Wrong number of arguments");
 
@@ -163,41 +100,28 @@ int main(int argc, char **argv) {
     s1 = argv[2];
     s2 = argv[3];
 
+    // Créer le nom du fichier de sortie
+    output_file = input_file + ".replace";
+
     infile.open(input_file.c_str());
     if (!infile.is_open())
         return print_error("Error opening the input file!");
-
-    outfile.open("output.txt");
+    outfile.open(output_file.c_str());
     if (!outfile.is_open())
         return print_error("Error opening the output file!");
-
-    std::string line;
-    while (getline(infile, line)) 
-	{
-        std::string new_line;
-        size_t pos = 0;
-        size_t start_pos = 0;
-
-        // line c'est la line actuel dans laquel je cherche
-		// s1 c'est le mot que je cherche dans line
-        while ((pos = line.find(s1, start_pos)) != std::string::npos) 
-		{
-            // Ajouter la partie avant s1
-            new_line += line.substr(start_pos, pos - start_pos);
-            // Ajouter s2 à la place de s1
-            new_line += s2;
-            // Mettre à jour start_pos pour continuer après la fin de s1
-            start_pos = pos + s1.length();
+    while (getline(infile, line))
+    {
+        pos = 0;
+        while ((pos = line.find(s1, pos)) != std::string::npos)
+	    {
+            line.erase(pos, s1.length()); // supprine s1 de la ligne
+            line.insert(pos, s2); // insert le contenu de s2 a la place
+            pos += s2.length(); // Avance la position pour éviter de trouver la même occurrence
         }
-
-        // Ajouter le reste de la ligne après la dernière occurrence de s1
-        new_line += line.substr(start_pos);
-
-        // Écrire la nouvelle ligne dans le fichier de sortie
-        outfile << new_line << std::endl;
+        outfile << line << std::endl;  // Écrire la ligne modifiée dans le fichier de sortie
     }
     infile.close();
     outfile.close();
 
-    return 0;
+    return (0);
 }
